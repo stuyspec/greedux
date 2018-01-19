9e3c8fd4786ebe15b3260ee384ff89ed698cfa81
Jason Kao <jasonkao85@gmail.com>
2018-01-19 11:15:08 -0500

    cleaned up

--- app/templates/src/js/modules/core/actions.js
+++ app/templates/src/js/modules/core/actions.js
@@ -1,13 +1,11 @@
-import * as t from './actionTypes'
+import * as t from "./actionTypes";
 
-export const setLanguage = (language)=>
-({
-    type    : t.SET_LANGUAGE,
-    payload : { language }
+export const setLanguage = language => ({
+  type: t.SET_LANGUAGE,
+  payload: { language },
 });
 
-export const refreshWindowDimensions = ()=>
-({
-    type : t.REFRESH_WINDOW_DIMENSIONS,
-    payload : {}
+export const refreshWindowDimensions = () => ({
+  type: t.REFRESH_WINDOW_DIMENSIONS,
+  payload: {},
 });
